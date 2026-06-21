bab47e95792c79b812b8f69da2a7bfec8e4d0fc3
ramon chuinca <ramonchuinca@hotmail.com>
2026-06-21 19:03:53 -0400

    construindo

--- src/main.js
+++ src/main.js
@@ -3,3 +3,12 @@ import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+import { createPinia } from 'pinia'
+
+const pinia = createPinia()
+
+createApp(App)
+  .use(router)
+  .use(pinia)
+  .mount('#app')
